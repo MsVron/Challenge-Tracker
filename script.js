8b9c819e0faef4f0aa8e5b1b00a5470097e1a44e
MsVron <126300537+MsVron@users.noreply.github.com>
2023-07-05 23:33:02 +0100

    Time

--- script.js
+++ script.js
@@ -1,112 +1,118 @@
-      // Retrieve the form and table elements
-      const form = document.getElementById('pushupForm');
-      const table = document.getElementById('progressTable');
 
-      // Load existing entries from local storage, if any
-      const storedEntries = localStorage.getItem('pushupEntries');
-      const pushupEntries = storedEntries ? JSON.parse(storedEntries) : [];
+// Retrieve the form and table elements
+const form = document.getElementById('pushupForm');
+const table = document.getElementById('progressTable');
 
-      // Function to add a new push-up entry
-      function addEntry(date, count) {
-        // Create a new entry object
-        const entry = {
-          date: date,
-          count: count
-        };
+// Load existing entries from local storage, if any
+const storedEntries = localStorage.getItem('pushupEntries');
+const pushupEntries = storedEntries ? JSON.parse(storedEntries) : [];
 
-        // Add the entry to the array
-        pushupEntries.push(entry);
+// Function to add a new push-up entry
+function addEntry(date, count) {
+    // Create a new entry object
+    const entry = {
+        date: date,
+        count: count
+    };
 
-        // Save the updated entries to local storage
-        localStorage.setItem('pushupEntries', JSON.stringify(pushupEntries));
-      }
+    // Add the entry to the array
+    pushupEntries.push(entry);
 
-      // Function to delete a push-up entry
-      function deleteEntry(index) {
-        // Remove the entry from the array
-        pushupEntries.splice(index, 1);
+    // Save the updated entries to local storage
+    localStorage.setItem('pushupEntries', JSON.stringify(pushupEntries));
+}
 
-        // Save the updated entries to local storage
-        localStorage.setItem('pushupEntries', JSON.stringify(pushupEntries));
+// Function to delete a push-up entry
+function deleteEntry(index) {
+    // Remove the entry from the array
+    pushupEntries.splice(index, 1);
 
-        // Render the entries
-        renderEntries();
-      }
+    // Save the updated entries to local storage
+    localStorage.setItem('pushupEntries', JSON.stringify(pushupEntries));
 
-      // Function to render the push-up entries in the table
-      function renderEntries() {
-        // Clear existing table rows
-        table.innerHTML = '<tr><th>Date</th><th>Push-Ups</th><th></th></tr>';
+    // Render the entries
+    renderEntries();
+}
 
-        // Iterate over the entries and create table rows
-        pushupEntries.forEach((entry, index) => {
-          const row = table.insertRow(-1);
-          const dateCell = row.insertCell(0);
-          const countCell = row.insertCell(1);
-          const deleteCell = row.insertCell(2);
 
-          dateCell.textContent = entry.date;
-          countCell.textContent = entry.count;
-          deleteCell.innerHTML = '<button onclick="deleteEntry(' + index + ')">Delete</button>';
-        });
-      }
+// Add event listener to the form for submitting entries
+form.addEventListener('submit', function (event) {
+    event.preventDefault(); // Prevent form submission
 
-      // Add event listener to the form for submitting entries
-      form.addEventListener('submit', function(event) {
-        event.preventDefault(); // Prevent form submission
+    // Get the input values
+    const date = document.getElementById('date').value;
+    const count = document.getElementById('count').value;
 
-        // Get the input values
-        const date = document.getElementById('date').value;
-        const count = document.getElementById('count').value;
+    // Add the entry and render the entries
+    addEntry(date, count);
+    renderEntries();
 
-        // Add the entry and render the entries
-        addEntry(date, count);
-        renderEntries();
+    // Clear the form inputs
+    document.getElementById('date').value = '';
+    document.getElementById('count').value = '';
+});
 
-        // Clear the form inputs
-        document.getElementById('date').value = '';
-        document.getElementById('count').value = '';
-      });
+// Set today's date as the default value for the date input
+const currentDate = new Date().toISOString().slice(0, 10);
+document.getElementById('date').value = currentDate;
 
-      // Set today's date as the default value for the date input
-      const currentDate = new Date().toISOString().slice(0, 10);
-      document.getElementById('date').value = currentDate;
+// Render the entries when the page loads
+renderEntries();
 
-      // Render the entries when the page loads
-      renderEntries();
-
-// Function to render the push-up entries in the table
 function renderEntries() {
-  // Clear existing table rows
-  table.innerHTML = '<tr><th>Date</th><th>Push-Ups</th><th></th></tr>';
+    // Clear existing table rows
+    table.innerHTML = '<tr><th>Date</th><th>Push-Ups</th><th></th></tr>';
 
-  // Iterate over the entries and create table rows
-  pushupEntries.forEach((entry, index) => {
-      const row = table.insertRow(-1);
-      const dateCell = row.insertCell(0);
-      const countCell = row.insertCell(1);
-      const deleteCell = row.insertCell(2);
+    // Iterate over the entries and create table rows
+    pushupEntries.forEach((entry, index) => {
+        const row = table.insertRow(-1);
+        const dateCell = row.insertCell(0);
+        const countCell = row.insertCell(1);
+        const deleteCell = row.insertCell(2);
 
-      const entryDate = new Date(entry.date);
-      const today = new Date();
+        const formattedDate = formatDate(entry.date);
+        dateCell.textContent = formattedDate;
+        countCell.textContent = entry.count;
+        deleteCell.innerHTML = `<button class="delete-button" onclick="deleteEntry(${index})">&#10006;</button>`;
+    });
+}
 
-      const diffTime = today - entryDate;
-      const diffDays = Math.floor(diffTime / (1000 * 60 * 60 * 24));
+function formatDate(dateString) {
+    const entryDate = new Date(dateString);
+    const today = new Date();
 
-      let formattedDate;
-      if (diffDays === 0) {
-          formattedDate = 'Today';
-      } else if (diffDays === 1) {
-          formattedDate = 'Yesterday';
-      } else if (diffDays < 7) {
-          const weekday = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'][entryDate.getDay()];
-          formattedDate = `${weekday} (${entry.date})`;
-      } else {
-          formattedDate = entry.date;
-      }
+    const diffTime = today - entryDate;
+    const diffDays = Math.floor(diffTime / (1000 * 60 * 60 * 24));
 
-      dateCell.textContent = formattedDate;
-      countCell.textContent = entry.count;
-      deleteCell.innerHTML = `<button onclick="deleteEntry(${index})">Delete</button>`;
-  });
+    if (diffDays === 0) {
+        return 'Today';
+    } else if (diffDays === 1) {
+        return 'Yesterday';
+    } else if (diffDays < 7 && today.getDay() !== 0 && entryDate.getDay() >= today.getDay()) {
+        const weekday = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'][entryDate.getDay()];
+        return `Last week on ${weekday}`;
+    } else if (diffDays < 7 && today.getDay() !== 0 && entryDate.getDay() < today.getDay()) {
+        const weekday = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'][entryDate.getDay()];
+        return `This week on ${weekday}`;
+    } else if (diffDays >= 7 && today.getDay() !== 0) {
+        const daysSinceLastSunday = (today.getDay() - 1 + 7) % 7;
+        const weekday = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'][entryDate.getDay()];
+
+        if (diffDays >= daysSinceLastSunday && diffDays < daysSinceLastSunday + 7) {
+            return `Last week on ${weekday}`;
+        } else if (diffDays >= daysSinceLastSunday + 7 && diffDays < daysSinceLastSunday + 14) {
+            return `Two weeks ago on ${weekday}`;
+        } else {
+            const day = String(entryDate.getDate()).padStart(2, '0');
+            const month = String(entryDate.getMonth() + 1).padStart(2, '0');
+            const year = entryDate.getFullYear();
+            return `${weekday} ${day}-${month}-${year}`;
+        }
+    } else {
+        const weekday = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'][entryDate.getDay()];
+        const day = String(entryDate.getDate()).padStart(2, '0');
+        const month = String(entryDate.getMonth() + 1).padStart(2, '0');
+        const year = entryDate.getFullYear();
+        return `${weekday} ${day}-${month}-${year}`;
+    }
 }
